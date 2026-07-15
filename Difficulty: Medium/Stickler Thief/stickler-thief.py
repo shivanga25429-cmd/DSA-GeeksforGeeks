ff4934ab645fd74141606355d42b3c5034dd4fc5
Shivang Agarwal <itshivang@gmail.com>
2026-07-15 17:44:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.py
@@ -0,0 +1,14 @@
+class Solution:  
+    def findMaxSum(self, nums):
+        # code here
+        n = len(nums)
+        if n==1:
+            return nums[0]
+        maxi = nums[0]
+        prev = nums[1]
+        for i in range(2,n):
+            curr = maxi + nums[i]
+            maxi = max(maxi,prev)
+            prev = curr
+        return max(prev,maxi)
+        
