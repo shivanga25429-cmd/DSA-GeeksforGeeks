a3f69eb07e960e3df5f029d43b63e5f739b77aed
Shivang Agarwal <itshivang@gmail.com>
2026-07-16 21:12:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, grid):
+        # code here 
+        if grid[0][0] == 1:
+            return 0
+        grid[0][0] = -1
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(1,n):
+            if grid[0][i] == 1:
+                grid[0][i] = 0
+                continue
+            else:
+                grid[0][i] += grid[0][i-1]
+        for i in range(1,m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    grid[i][j] = 0
+                    continue
+                if j > 0:
+                    grid[i][j] += grid[i][j-1]
+                grid[i][j] += grid[i-1][j]
+        return -grid[-1][-1]
