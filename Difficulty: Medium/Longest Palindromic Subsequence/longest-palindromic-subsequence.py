bad5e7dd6c9df8f743c532aece5721ad1a7bf270
Shivang Agarwal <itshivang@gmail.com>
2026-09-07 22:52:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalinSubseq(self, s):
+        # code here
+        n = len(s)
+        prev = [0]*(n+1)
+        for i in range(1,n+1):
+            curr = [0]*(n+1)
+            for j in range(1,n+1):
+                if s[i-1] == s[n-j]:
+                    curr[j] = 1 + prev[j-1]
+                else:
+                    curr[j] = max(prev[j], curr[j-1])
+            prev = curr
+        return prev[-1]
